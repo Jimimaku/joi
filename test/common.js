f9f9c32f60c88a619de5a301277b8beb999f1a97
Nicolas Morel <nicolas@morel.io>
2026-02-10 19:49:33 +0100

    feat: add Standard JSON Schema

--- test/common.js
+++ test/common.js
@@ -22,4 +22,21 @@ describe('Common', () => {
             expect(() => Common.assertOptions()).to.throw('Options must be of type object');
         });
     });
+
+    describe('intersect', () => {
+
+        it('intersects two sets', () => {
+
+            const a = new Set([1, 2, 3]);
+            const b = new Set([2, 3, 4]);
+            expect(Common.intersect(a, b)).to.equal(new Set([2, 3]));
+        });
+
+        it('returns empty set when no intersection', () => {
+
+            const a = new Set([1, 2]);
+            const b = new Set([3, 4]);
+            expect(Common.intersect(a, b)).to.equal(new Set());
+        });
+    });
 });
